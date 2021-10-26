f7ae4c6f245f4d6d10616244a4732aa2c3132544
Agustin <winecod3@gmail.com>
2021-10-26 14:07:12 -0400

    pruebalogin

---
+++ src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule, FormsModule} from '@angular/forms';
+import { NgxSpinnerModule } from "ngx-spinner";
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
+import { AngularFirestore } from '@angular/fire/firestore';
+
+import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { MensajesService } from './services/mensajes.service';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    EncabezadoComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AccordionModule.forRoot(),
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    NgxSpinnerModule,
+    FormsModule,
+    AngularFireStorageModule,
+    ProgressbarModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    CollapseModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebase)
+  ],
+  providers: [
+    AngularFireAuth,
+    AngularFirestore,
+    MensajesService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  firebase: {
+      apiKey: "AIzaSyAMW-tqpBFfTE3JPEXQXwp0z6TgWZW95ko",
+      authDomain: "testlogin-a6198.firebaseapp.com",
+      projectId: "testlogin-a6198",
+      storageBucket: "testlogin-a6198.appspot.com",
+      messagingSenderId: "185254025217",
+      appId: "1:185254025217:web:854f301862d36bdeaa8800",
+      measurementId: "G-GZNY483YEV"
+  }
+};
+
+//Esto lo encontraran en clase Agregar AngularFire2 del proyecto final
